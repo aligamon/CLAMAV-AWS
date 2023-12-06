19810edb49ff9501cb8eb8d4545c7d3d562e79d2
aligamon <aligamon96@gmail.com>
2023-12-06 10:40:11 +0800

    add description

--- lambda/index.mjs
+++ lambda/index.mjs
@@ -7,14 +7,14 @@ export const handler = async (event, context) => {
     const S3 = new S3Client();
 
     try {
-        const listObjectsResponse = await S3.send(new ListObjectsCommand({
+        const listObjectsResponse = await S3.send(new ListObjectsCommand({ // retrieves a list of objects from a specified Amazon S3 bucket.
             Bucket: source_bucket
         }));
 
         for (const obj of listObjectsResponse.Contents || []) {
             const key = obj.Key;
 
-            const tagResponse = await S3.send(new GetObjectTaggingCommand({
+            const tagResponse = await S3.send(new GetObjectTaggingCommand({ //retrieves the tags associated with a specific object in an Amazon S3 bucket.
                 Bucket: source_bucket,
                 Key: key
             }));
@@ -25,7 +25,7 @@ export const handler = async (event, context) => {
 
                 if (scanStatus === 'CLEAN') {
                     const destinationKey = key;
-                    await S3.send(new CopyObjectCommand({
+                    await S3.send(new CopyObjectCommand({ //copies "CLEAN" files from a source location to a different Amazon S3 bucket.
                         Bucket: destination_bucket,
                         CopySource: `${source_bucket}/${key}`,
                         Key: destinationKey
@@ -33,7 +33,7 @@ export const handler = async (event, context) => {
 
                     console.log(`File ${key} copied to ${destination_bucket}`);
                 } else if (scanStatus === 'INFECTED') {
-                    await S3.send(new DeleteObjectCommand({ Bucket: source_bucket, Key: key }));
+                    await S3.send(new DeleteObjectCommand({ Bucket: source_bucket, Key: key })); //deletes "INFECTED" files from Amazon S3 with scanning capabilities.
 
                     console.log(`Infected file ${key} deleted from ${source_bucket}`);
                 }
